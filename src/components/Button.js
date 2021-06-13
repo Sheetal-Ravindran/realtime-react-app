19128d0d1ac7f2e6fa704e295cf799edd6b53737
Sheetal Ravindran <sheetalpr30@gmail.com>
2021-06-13 10:36:36 +0530

    Add disable style for button

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,6 @@ class Button extends React.Component {
     
     render()  {  
       const buttonClassName = this.props.type === "Primary" ? "primary" : "secondary";
-      console.log(buttonClassName);
         return (
           <div  className="form-group">
             <button className={buttonClassName} disabled={this.props.disabled}>{this.props.content}</button>                 
