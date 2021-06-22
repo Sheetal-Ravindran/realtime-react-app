ba2e58cdbcbaff8ca26c743c6f208e2c117204c2
Sheetal Ravindran <sheetalpr30@gmail.com>
2021-06-22 22:20:49 +0530

    Remove commented code

--- src/index.js
+++ src/index.js
@@ -7,16 +7,7 @@ class App extends React.Component {
  
   render() {
    
-    return (
-      // <div className="validation"> 
-      //     <h2 className="header">Basic Details</h2>      
-      //     <FormInput label="Enter First Name " type="text"/>
-      //     <FormInput label="Enter Last Name " type="text"/>
-      //     <div>
-      //       <Button type="primary" content="Primary" disabled={false}/> 
-      //       <Button type="secondary" content="Secondary" disabled={false}/>
-      //     </div>         
-      // </div>
+    return (    
       <Register/>
     );
   }
